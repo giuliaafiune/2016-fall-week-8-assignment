73ee6d4c5f8628f1dca6b997ffd17b462b176cc6
Giulia Afiune <giuliaafiune@Giulias-MacBook-Pro.local>
2016-12-16 22:04:57 -0200

    Submission

--- 8.0/script.js
+++ 8.0/script.js
@@ -13,7 +13,7 @@ var plot = d3.select('.canvas')
     .append('g')
     .attr('transform','translate(' + m.l + ',' + m.t + ')');
 
-//d3.set to hold a unique array of airlines
+//d3.set creates a new array with just unique airline names because we are grouping flights by airline
 var airlines = d3.set();
 
 //Scale
@@ -35,13 +35,15 @@ var axisY = d3.axisLeft()
 
 //Line generator
 var lineGenerator = d3.line()
-    .x(function(d){return scaleX(d.year)})
-    .y(function(d){return scaleY(d.value)})
+    .x(function(d){return scaleX(d.travelDate)})
+    .y(function(d){return scaleY(d.price)})
     .curve(d3.curveCardinal);
 
 d3.queue()
     .defer(d3.csv, '../data/bos-sfo-flight-fare.csv',parse)
     .await(function(err, data){
+
+        console.log(data);
 
         //Mine the data to set the scales
         scaleX.domain( d3.extent(data,function(d){return d.travelDate}) );
@@ -50,16 +52,23 @@ d3.queue()
         //Add buttons
         d3.select('.btn-group')
             .selectAll('.btn')
-            .data( airlines.values() )
+            .data( airlines.values() , function(d){ return d.id; }) // this interprets the id column as the key column
             .enter()
             .append('a')
-            .html(function(d){return d})
+            .html(function(d){return d;})
             .attr('href','#')
             .attr('class','btn btn-default')
             .style('color','white')
             .style('background',function(d){return scaleColor(d)})
             .style('border-color','white')
             .on('click',function(d){
+
+                draw(data.filter( function (d) { 
+
+                    return d.airline = d.key;
+
+                }));
+
                 //Hint: how do we filter flights for particular airlines?
                 //data.filter(...)
 
@@ -78,6 +87,7 @@ d3.queue()
     });
 
 function draw(rows){
+
     //IMPORTANT: data transformation
     var flightsByTravelDate = d3.nest().key(function(d){return d.travelDate})
         .entries(rows);
@@ -87,17 +97,98 @@ function draw(rows){
     });
 
     console.log(flightsByTravelDate);
+    console.log(rows);
 
     //Draw dots
 
+    var nodes = plot.selectAll(".dot")
+        .data(rows) // UPDATE 
+
+    //Enter
+    var nodesEnter = nodes.enter() //ENTER
+        .append("circle")
+        .attr("class","dot");
+
+    //Exit
+    nodes.exit().remove();
+
+    //Update
+    var nodesTransition = nodes
+        .merge(nodesEnter) // UPDATE+ENTER
+        .transition().duration(1000)
+        .attr("r",5)
+        .attr('cx',function(d){return scaleX(d.travelDate)})
+        .attr('cy',function(d){return scaleY(d.price)})
+        .style('fill',function(d){return scaleColor(d.airline)})
+        .on('mouseenter',function(d){
+            var tooltip = d3.select('.custom-tooltip'); // creates a new div with this class. It cointains two other divs (title and value)
+            tooltip.select('.title')
+                .html("This flight's price is")
+            tooltip.select('.value')
+                .html(d.price);
+
+            tooltip.transition().style('opacity',1);
+
+            d3.select(this).style('stroke-width','3px');
+        })
+        .on('mousemove',function(d){
+            var tooltip = d3.select('.custom-tooltip');
+            var xy = d3.mouse( d3.select('.container').node() );
+            tooltip
+                .style('left',xy[0]+10+'px')
+                .style('top',xy[1]+10+'px');
+        })
+        .on('mouseleave',function(d){
+            var tooltip = d3.select('.custom-tooltip');
+            tooltip.transition().style('opacity',0);
+
+            d3.select(this).style('stroke-width','0px');
+        })
+        .on("click", function (d) { 
+
+            draw(data.filter( function (d) { 
+
+                return d.airline = d.key;
+
+            }));
+
+
+            var tooltip = d3.select('.custom-tooltip');
+            tooltip.select('.title')
+                .html(d.airline)
+            tooltip.select('.value')
+                .html(d.price);
+            var xy = d3.mouse( d3.select('.container').node() );
+            tooltip
+                .style('left',xy[0]+10+'px')
+                .style('top',xy[1]+10+'px');
+        });
+
 
     //Draw <path>
+
+
+    var path = plot.selectAll(".time-series")
+        .datum(rows)
+        .append("path")
+        .transition()
+        .attr("class","time-series")
+        .attr('d',function(datum){
+            return lineGenerator(datum.sort());
+        })
+        .style('fill','none')
+        .style('stroke-width','2px')
+        .style('stroke',function(datum){
+            return scaleColor(datum[0].airline);
+        });
+
 }
+
 
 function parse(d){
 
     if( !airlines.has(d.airline) ){
-        airlines.add(d.airline);
+        airlines.add(d.airline);    // this populates the new array that we created on the top. Creates the new unique airline array
     }
 
     return {
